05a571e518b350b197fd5400c5f397d839460a9c
SOHick <shnikitaprog@mail.ru>
2022-11-18 16:45:13 +0300

    New "ClassC"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет!");
         System.out.println("Значение класса A:");
+        ClassC c = new ClassC();
+        String w = "Максим лучший командир!!!";
+        c.Maksim(w);
         ClassA a = new ClassA(3);
         System.out.println(a);
     }
