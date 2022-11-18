deca52831dc63e3571905deb1c2e0ad0e51b9ca9
Sergey Makletsov <smaklets@kpfu.ru>
2022-11-18 16:16:15 +0300

    Создание объекта класса A и вывод его состояния

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет!");
+        ClassA a = new ClassA(3);
+        System.out.println(a);
     }
 }
